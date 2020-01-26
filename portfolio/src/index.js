9ec6b40707ea2bdb280a7d9247658183bce19733
Justin Cheng <ijustin17@gmail.com>
2020-01-26 00:09:03 -0600

    added a nav bar and card components

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,10 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// importing components
+import Nav from "./components/nav";
+import CardContent from "./components/card";
+import App from "./App";
+
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(
+  <div>
+    <Nav />,
+    <div className="card-deck">
+      <div class="card">
+        <CardContent />,
+      </div>
+      <div class="card">
+        <CardContent />,
+      </div>
+      <div class="card">
+        <CardContent />,
+      </div>
+      <div class="card">
+        <CardContent />,
+      </div>
+    </div>
+  </div>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
